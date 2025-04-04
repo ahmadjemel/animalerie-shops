cc96b6e1113b6b38ba986eb23f02a2257c25ef90
ahmadjemel <ahmad.jemel10@gmail.com>
2025-04-04 22:08:17 +0100

    animalerie-shops

---
+++ script.js
@@ -0,0 +1,30 @@
+// Exemple de fonction pour le formulaire de contact
+/* document.getElementById('form-contact').addEventListener('submit', function (event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    const nom = document.getElementById('nom').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Merci ${nom}, votre message a été envoyé !`);
+    // Ici, vous pouvez ajouter une logique pour envoyer les données à un serveur
+}); */
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+})
+document.addEventListener('scroll', function () {
+    const scrollButton = document.querySelector('.scroll');
+    if (window.scrollY > 300) { 
+        scrollButton.classList.add('visible');
+    } else {
+        scrollButton.classList.remove('visible');
+    }
+});
+document.querySelector('.scroll').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
